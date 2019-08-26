590d4ba5b3945c38408666059c84439d16ebb18c
apinuntong <apinun.tong@gmail.com>
2019-08-27 01:33:30 +0700

    Add files via upload

--- blocks/blocks_linear-hall.js
+++ blocks/blocks_linear-hall.js
@@ -2,15 +2,13 @@ Blockly.Blocks['linear_hall_block'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("LINEAR HALL");
-    this.appendValueInput("LINEAR_HALL_ANALOG_PIN")
+    this.appendValueInput("LINEAR_HALL_PIN")
         .setCheck("Number")
-        .appendField("ANALOG PIN");
-    this.appendValueInput("LINEAR_HALL_DIGITAL_PIN")
-        .setCheck("Number")
-        .appendField("DIGITAL PIN");
+        .appendField("PIN");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["digital","0"], ["analog","1"]]), "ST_PIN");
     this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+    this.setOutput(true, "Number");
     this.setColour(230);
  this.setTooltip("");
  this.setHelpUrl("");
--- blocks/generators_linear-hall.js
+++ blocks/generators_linear-hall.js
@@ -1,7 +1,14 @@
 Blockly.JavaScript['linear_hall_block'] = function(block) {
-  var value_linear_hall_analog_pin = Blockly.JavaScript.valueToCode(block, 'LINEAR_HALL_ANALOG_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_linear_hall_digital_pin = Blockly.JavaScript.valueToCode(block, 'LINEAR_HALL_DIGITAL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_linear_hall_pin = Blockly.JavaScript.valueToCode(block, 'LINEAR_HALL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_st_pin = block.getFieldValue('ST_PIN');
   // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
+  if(dropdown_st_pin == "0"){
+  	  var code =
+  `#SETUP pinMode(${value_linear_hall_pin}, INPUT);#ENDdigitalRead(${value_linear_hall_pin})`
+  }
+  if(dropdown_st_pin == "1"){
+  	var code =`analogRead(${value_linear_hall_pin})`
+  }
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
